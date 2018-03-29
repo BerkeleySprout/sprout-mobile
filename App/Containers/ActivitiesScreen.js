fbde612b3093e208b4bc0e34769a03692d6208bb
Tao Ong <taoong@berkeley.edu>
2018-03-29 15:26:22 -0700

    Removing unnecessary comments.

--- App/Containers/ActivitiesScreen.js
+++ App/Containers/ActivitiesScreen.js
@@ -19,11 +19,6 @@ export default class ActivitiesScreen extends React.PureComponent {
     tabBarIcon: ({ tintColor }) => <Icon name="heart" size={20} color={tintColor}/>
   }
 
-  /* ***********************************************************
-  * STEP 1
-  * This is an array of objects with the properties you desire
-  * Usually this should come from Redux mapStateToProps
-  *************************************************************/
   state = {
     data: [
     {
@@ -49,21 +44,6 @@ export default class ActivitiesScreen extends React.PureComponent {
     ]
   }
 
-
-  /* ***********************************************************
-  * STEP 3
-  * `renderItem` function - How each cell should be rendered
-  * It's our best practice to place a single component here:
-  *
-  * e.g.
-  *   return <MyCustomCell title={item.title} description={item.description} />
-  *
-  * For sections with different cells (heterogeneous lists), you can do branch
-  * logic here based on section.key OR at the data level, you can provide
-  * `renderItem` functions in each section.
-  *
-  * Note: You can remove section/separator functions and jam them in here
-  *************************************************************/
   renderItem ({section, item}) {
     const gradientHeight = 100
     const gradientBackground = 'black'
@@ -104,31 +84,10 @@ export default class ActivitiesScreen extends React.PureComponent {
   renderEmpty = () =>
     <Text style={styles.label}> - Nothing to See Here - </Text>
 
-  renderSeparator = () =>
-    <Text style={styles.label}></Text>
-
-  // The default function if no Key is provided is index
-  // an identifiable key is important if you plan on
-  // item reordering.  Otherwise index is fine
   keyExtractor = (item, index) => index
 
-  // How many items should be kept im memory as we scroll?
   oneScreensWorth = 20
-
-  // extraData is for anything that is not indicated in data
-  // for instance, if you kept "favorites" in `this.state.favs`
-  // pass that in, so changes in favorites will cause a re-render
-  // and your renderItem will have access to change depending on state
-  // e.g. `extraData`={this.state.favs}
-
-  // Optimize your list if the height of each item can be calculated
-  // by supplying a constant height, there is no need to measure each
-  // item after it renders.  This can save significant time for lists
-  // of a size 100+
-  // e.g. itemLayout={(data, index) => (
-  //   {length: ITEM_HEIGHT, offset: ITEM_HEIGHT * index, index}
-  // )}
-
+  
   render () {
     return (
       <View style={styles.container}>
