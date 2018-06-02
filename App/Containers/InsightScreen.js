e1fafa0adc76756961716bccc22c05a08c3327ef
Tao Ong <taoong@berkeley.edu>
2018-06-01 19:03:40 -0700

    Adding more example data to insights to test scrolling

--- App/Containers/InsightScreen.js
+++ App/Containers/InsightScreen.js
@@ -15,7 +15,17 @@ export default class InsightScreen extends Component {
       {time: '10:45', title: 'Event 2', description: 'Event 2 Description'},
       {time: '12:00', title: 'Event 3', description: 'Event 3 Description'},
       {time: '14:00', title: 'Event 4', description: 'Event 4 Description'},
-      {time: '16:30', title: 'Event 5', description: 'Event 5 Description'}
+      {time: '16:30', title: 'Event 5', description: 'Event 5 Description'},
+      {time: '09:00', title: 'Event 6', description: 'Event 6 Description'},
+      {time: '10:45', title: 'Event 7', description: 'Event 7 Description'},
+      {time: '12:00', title: 'Event 8', description: 'Event 8 Description'},
+      {time: '14:00', title: 'Event 9', description: 'Event 9 Description'},
+      {time: '16:30', title: 'Event 10', description: 'Event 10 Description'},
+      {time: '09:00', title: 'Event 11', description: 'Event 11 Description'},
+      {time: '10:45', title: 'Event 12', description: 'Event 12 Description'},
+      {time: '12:00', title: 'Event 13', description: 'Event 13 Description'},
+      {time: '14:00', title: 'Event 14', description: 'Event 14 Description'},
+      {time: '16:30', title: 'Event 15', description: 'Event 15 Description'},
     ]
   }
 	
