19c14d944a707c7038c1fca02bc15e2ae9b8f638
Tao Ong <taoong@berkeley.edu>
2018-07-12 23:06:13 -0700

    Changing insight page circle colors

--- App/Containers/InsightScreen.js
+++ App/Containers/InsightScreen.js
@@ -48,7 +48,7 @@ export default class InsightScreen extends Component {
           <Timeline
             data={this.data}
             circleSize={20}
-            circleColor={Colors.green2}
+            circleColor={Colors.green}
             innerCircle={'dot'}
             lineColor={Colors.cloud}
             timeStyle={{textAlign: 'center', backgroundColor: Colors.green2, color:'white', padding:5, borderRadius:13, overflow: 'hidden'}}
