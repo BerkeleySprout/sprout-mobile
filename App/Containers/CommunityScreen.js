5cbe92035bf7b200659236ff11a929be3c53c257
Tao Ong <taoong@berkeley.edu>
2018-08-03 22:05:29 -0700

    Users now being fetched from firebase

--- App/Containers/CommunityScreen.js
+++ App/Containers/CommunityScreen.js
@@ -11,8 +11,9 @@ class CommunityScreen extends React.PureComponent {
   constructor () {
     super()
     this.state = {
-      selectedIndex: 2,
-      users: []
+      selectedIndex: 1,
+      users: [],
+      refreshing: false
     }
     this.updateIndex = this.updateIndex.bind(this)
   }
@@ -21,69 +22,17 @@ class CommunityScreen extends React.PureComponent {
     this.setState({selectedIndex})
   }
 
+  _onRefresh = () => {
+    this.setState({refreshing: true});
+    fetchData().then(() => {
+      this.setState({refreshing: false});
+    });
+  }
+
   static navigationOptions = {
     tabBarLabel: "Community",
     tabBarIcon: ({ tintColor }) => <Icon name="users" size={20} color={tintColor}/>
   }
-
-  state = {
-    data: [
-      {
-        key: 'First',
-        data: [
-          {title: 'First Title', description: 'First Description'},
-          {title: 'Second Title', description: 'Second Description'},
-          {title: 'Third Title', description: 'Third Description'},
-          {title: 'Fourth Title', description: 'Fourth Description'},
-          {title: 'Fifth Title', description: 'Fifth Description'},
-          {title: 'Sixth Title', description: 'Sixth Description'},
-          {title: 'Seventh Title', description: 'Seventh Description'},
-          {title: 'Eighth Title', description: 'Eighth Description'},
-          {title: 'Ninth Title', description: 'Ninth Description'},
-          {title: 'Tenth Title', description: 'Tenth Description'}
-        ]
-      }, {
-        key: 'Second',
-        data: [
-          {title: 'Eleventh Title', description: 'Eleventh Description'},
-          {title: '12th Title', description: '12th Description'},
-          {title: '13th Title', description: '13th Description'},
-          {title: '14th Title', description: '14th Description'},
-          {title: '15th Title', description: '15th Description'},
-          {title: '16th Title', description: '16th Description'},
-          {title: '17th Title', description: '17th Description'},
-          {title: '18th Title', description: '18th Description'},
-          {title: '19th Title', description: '19th Description'},
-          {title: '20th Title', description: '20th Description'},
-          {title: 'BLACKJACK!', description: 'BLACKJACK! Description'}
-        ]
-      }
-    ]
-  }
-
-  renderItem ({section, item}) {
-    return (
-      <View style={styles.row}>
-        <Text style={styles.boldLabel}>{item.title}</Text>
-        <Text style={styles.label}>{item.description}</Text>
-      </View>
-    )
-  }
-
-  renderSectionHeader ({section}) {
-    switch (section.key) {
-      case 'First':
-        return <View><Text style={styles.boldLabel}>First Section</Text></View>
-      default:
-        return <View><Text style={styles.boldLabel}>Second Section</Text></View>
-    }
-  }
-
-  renderHeader = () =>
-    <Text style={[styles.label, styles.sectionHeader]}> - Full List Header - </Text>
-
-  renderEmpty = () =>
-    <Text style={styles.label}> - Nothing to See Here - </Text>
 
   keyExtractor = (item, index) => index
 
@@ -95,25 +44,15 @@ class CommunityScreen extends React.PureComponent {
 
   render () {
 
-    const list = [
-      {
-        name: 'Amy Farha',
-        image: 'https://s3.amazonaws.com/uifaces/faces/twitter/ladylexy/128.jpg',
-        subtitle: 'Vice President'
-      },
-      {
-        name: 'Chris Jackson',
-        image: 'https://s3.amazonaws.com/uifaces/faces/twitter/adhamdannaway/128.jpg',
-        subtitle: 'Vice Chairman'
-      },
-    ]
+    console.log("reloaded")
 
     var database = firebase.database();
     database.ref("users/").once("value", snapshot => {
       var userSnapshot = snapshot.val()
+      var tempUsers = [];
       for (user in userSnapshot) {
-        list.push(userSnapshot[user])
-        this.state.users.push(userSnapshot[user])
+        tempUsers.push(userSnapshot[user])
+        this.state.users = tempUsers
       }
       this.forceUpdate()
     })
@@ -146,7 +85,7 @@ class CommunityScreen extends React.PureComponent {
         <ScrollView>
         <List containerStyle={{marginBottom: 20}}>
           {
-            list.map((l, i) => (
+            this.state.users.map((l, i) => (
               <TouchableOpacity
                 key={i}>
                 <ListItem
